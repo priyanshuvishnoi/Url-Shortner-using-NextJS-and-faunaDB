0d7b085d987d0889beadb6e352129587aec9949f
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-07-20 00:43:14 +0530

    test

--- pages/api/url/[id].js
+++ pages/api/url/[id].js
@@ -3,5 +3,6 @@ import { getUrl } from '../../../utils/fauna';
 export default async function handler(req, res) {
   const { id } = req.query;
   const url = await getUrl(id);
+  console.table({ url: url.longUrl, id });
   res.redirect(url.longUrl);
 }

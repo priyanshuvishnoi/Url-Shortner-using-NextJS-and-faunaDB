07f4869740b9378b9c637eeb534f4c9657de850a
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-07-20 09:41:32 +0530

    test

--- pages/api/shortenUrl.js
+++ pages/api/shortenUrl.js
@@ -1,12 +1,12 @@
 import { saveUrl } from '../../utils/fauna';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { url } = req.body;
     const code = 'xxxxxxx'.replace(/x/g, () =>
       Math.floor(Math.random() * 16).toString(16)
     );
+    await saveUrl(code, url);
     res.status(200).json({ url: `${req.headers.host}/api/url/${code}` });
-    saveUrl(code, url);
   } else res.status(500).send({ message: 'Only POST method is allowed' });
 }
--- utils/fauna.js
+++ utils/fauna.js
@@ -3,18 +3,26 @@ const faunaClient = new faunaDB.Client({ secret: process.env.FAUNADB_SECRET });
 const q = faunaDB.query;
 
 const saveUrl = async (id, longUrl) => {
-  return await faunaClient.query(
-    q.Create(q.Collection('urls'), {
-      data: { smallUrl: id, longUrl },
-    })
-  );
+  try {
+    return await faunaClient.query(
+      q.Create(q.Collection('urls'), {
+        data: { smallUrl: id, longUrl },
+      })
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getUrl = async id => {
-  const snippet = await faunaClient.query(
-    q.Select(['data'], q.Get(q.Match(q.Index('smallUrl'), id)))
-  );
-  return snippet;
+  try {
+    const snippet = await faunaClient.query(
+      q.Select(['data'], q.Get(q.Match(q.Index('smallUrl'), id)))
+    );
+    return snippet;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
